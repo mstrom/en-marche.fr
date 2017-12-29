c808e915b4e1c11a1d705e02e6f2cf1c4a8e083a
Rémi Gardien <remi.gardien@sensiolabs.com>
2017-12-29 17:34:59 +0100

    Remove useless use statements

--- src/Form/TimelineThemeMeasureType.php
+++ src/Form/TimelineThemeMeasureType.php
@@ -2,20 +2,13 @@
 
 namespace AppBundle\Form;
 
-use AppBundle\Entity\BaseGroup;
-use AppBundle\Entity\CoordinatorAreaInterface;
 use AppBundle\Entity\Timeline\Measure;
 use AppBundle\Entity\Timeline\ThemeMeasure;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TimelineThemeMeasureType extends AbstractType
 {
