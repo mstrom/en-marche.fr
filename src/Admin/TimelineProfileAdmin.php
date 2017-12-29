51cfad43d53ff21c7377ca1a6980f57cd54704b4
Rémi Gardien <remi.gardien@sensiolabs.com>
2017-12-29 17:29:14 +0100

    Fix fixtures

--- src/Admin/TimelineProfileAdmin.php
+++ src/Admin/TimelineProfileAdmin.php
@@ -6,7 +6,6 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
